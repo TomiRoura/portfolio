5f222b2426c5fca6e0b95ff73d4795059e1231ab
TomiRoura <111542351+TomiRoura@users.noreply.github.com>
2022-10-21 14:33:55 -0300

    navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "963648788357b7d3"; }
+/******/ 		__webpack_require__.h = function() { return "be431846f43289cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
