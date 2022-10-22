5a45c4820b9057211a1bc361103c331b9983d198
TomiRoura <111542351+TomiRoura@users.noreply.github.com>
2022-10-22 20:52:44 -0300

    22/oct

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "be431846f43289cf"; }
+/******/ 		__webpack_require__.h = function() { return "7a2f190aa472b455"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@ import styles from './/Navbar.module.scss';
 import Link from 'next/link';
 import logo from '../src/logo.webp';
 
-
 const Navbar: NextPage = () => {
     return (
         <main>
@@ -11,10 +10,17 @@ const Navbar: NextPage = () => {
 
                 <div className={styles.navbarLinks}>
                     <h1><Link href="/"><img src={logo.src} className={styles.Logo} /></Link> </h1>
+                    <h1 className={styles.homeLink}><Link href="/">home</Link></h1>
                     <h1 className={styles.aboutLink}><Link href="/about">about</Link></h1>
                     <h1 className={styles.portfolioLink}><Link href="/portfolio">portfolio</Link></h1>
                 </div>
+                <button className={styles.hamburger}>
+                    <span className={styles.hamburguer_bar}></span>
+                    <span className={styles.hamburguer_bar}></span>
+                    <span className={styles.hamburguer_bar}></span>
+                </button>
             </div>
+
         </main>
     )
 }
